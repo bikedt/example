acde17d9a41065ed6382bc39ac2a25bcb5819696
bikedt <bikedogtrade@gmail.com>
2017-04-02 14:02:13 -0700

    dropdown attempt at fix style.css and script

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 // Empty JS for your own code to be here
+$('.dropdown-toggle').click(function(e) {
+  e.preventDefault();
+  setTimeout($.proxy(function() {
+    if ('ontouchstart' in document.documentElement) {
+      $(this).siblings('.dropdown-backdrop').off().remove();
+    }
+  }, this), 0);
+});
